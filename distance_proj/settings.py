a128869555f23ebd64ecabccb545cdc9b3e20718
faisalzone <faisalzone@gmail.com>
2021-01-07 01:19:24 +1100

    points defined and data from ip captured

--- distance_proj/settings.py
+++ distance_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +117,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- measurements/views.py
+++ measurements/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Measurement
 from .forms import MeasurementModelForm
+# https://pypi.org/project/geopy/
+# https://pypi.org/project/geoip2/
+from geopy.geocoders import Photon
+# For calculating distance between two variables
+from geopy.distance import geodesic
+from .utils import get_geo
+
 
 # Create your views here.
 
@@ -8,12 +15,39 @@ from .forms import MeasurementModelForm
 def calculate_distance_view(request):
     obj = get_object_or_404(Measurement, id=1)
     form = MeasurementModelForm(request.POST or None)
+    geolocator = Photon(user_agent="measurements")
+
+    ip = '72.14.207.99'
+    country, city, lat, lon = get_geo(ip)
+    # print('location country', country)
+    # print('location city', city)
+    # print('location lat, lon', lat, lon)
+
+    location = geolocator.geocode(city)
+    # print('###', location)
+
+    l_lat = lat
+    l_lon = lon
+    pointA = (l_lat, l_lon)
 
     if form.is_valid():
         instance = form.save(commit=False)
-        instance.destination = form.cleaned_data.get('destination')
-        instance.location = 'San Francisco'
-        instance.distance = 5000.00
+        destination_ = form.cleaned_data.get('destination')
+        destination = geolocator.geocode(destination_)
+        # print(destination)
+        # print(destination.latitude)
+        # print(destination.longitude)
+        d_lat = destination.latitude
+        d_lon = destination.longitude
+
+        pointB = (d_lat, d_lon)
+
+        distance = round(geodesic(pointA, pointB).km, 2)
+
+        instance.location = location
+        instance.distance = distance
+        # instance.location = 'San Francisco'
+        # instance.distance = 5000.00
         instance.save()
 
     context = {
